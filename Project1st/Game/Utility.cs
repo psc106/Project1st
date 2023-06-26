ee1df571c7a2c0afea3ee2bc71c26a0f6878e8db
DESKTOP-484M8JG\KGA <psc106@naver.com>
2023-06-26 12:31:18 +0900

    Init : 유틸리티 클래스 생성
    게임을 구성하는 요소가 아닌 기능중에 static으로 쓰고싶은 기능을 모아놓은 유틸리티 클래스 생성

---
+++ Project1st/Game/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1st.Game
+{
+    public class Coordinate
+    {
+        public int x;
+        public int y;
+
+        public Coordinate() { }
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
