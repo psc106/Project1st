23dc5d60c990464e964562ed60a950fae34ce091
DESKTOP-484M8JG\KGA <psc106@naver.com>
2023-06-26 12:26:12 +0900

    Init : 부모 객체 클래스 생성
    맵에서 표현될 객체가 상속할 부모 객체 클래스 생성

---
+++ Project1st/Game/GameObject/MainObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1st.Game.GameObject
+{
+    public class MainObject
+    {
+        public Coordinate Axis2D;
+    }
+
+    public class MoveObject : MainObject
+    {
+        public int direction { get; set; }
+    }
+}
