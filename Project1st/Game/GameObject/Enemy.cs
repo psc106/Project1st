b57c45d6fe720c8ad823e6d52140a08ed84c8aa1
DESKTOP-484M8JG\KGA <psc106@naver.com>
2023-06-27 09:03:55 +0900

    Feat : 플레이어, 적 객체에 기본 기능 추가
    초기화, 실시간이동 기능 추가

--- Project1st/Game/GameObject/Enemy.cs
+++ Project1st/Game/GameObject/Enemy.cs
@@ -1,14 +1,79 @@
-﻿using System;
+﻿using Project1st.Game.Algorithm;
+using Project1st.Game.Core;
+using Project1st.Game.Map;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Project1st.Game.GameObject
 {
     public class Enemy : MoveObject
     {
-        int id;
+        static public int EnemyHitPointMAX = 50;
 
+        public Timer moveTimer;
+
+        List<Location> path;
+        public bool isMove;
+
+        public Enemy()
+        {
+            base.Init();
+            hitPoint = 100;
+            ID = 0;
+            isMove = false;
+            path = new List<Location>();
+        }
+
+
+        public void StartTimer()
+        {
+            moveTimer = new Timer(MoveTimer, null, 3000, 600 - (10 * (EnemyHitPointMAX - hitPoint)));
+
+        }
+
+        public void MoveTimer(object obj)
+        {
+            if (!isLive)
+            {
+                isLive = true;
+            }
+
+            if (isMove)
+            {
+                path = EAlgorithm.Go(this);
+                isMove = false;
+            }
+            if (path == null || path.Count == 0) return;
+            Location next = path[0];
+
+            if (GameManger.currField.GetElementAt(next.X, next.Y) != Field.field_info.wall &&
+                GameManger.currField.FindEnemiesAt(next.X, next.Y) == null)
+            {
+                this.Axis2D.x = next.X;
+                this.Axis2D.y = next.Y;
+                path.RemoveAt(0);
+            }
+            else
+            {
+            }
+
+            if (GameManger.player.Axis2D.x == this.Axis2D.x && GameManger.player.Axis2D.y == this.Axis2D.y)
+            {
+                GameManger.player.hitPoint -= 1;
+                moveTimer.Dispose();
+                if (GameManger.player.hitPoint == 0)
+                {
+                    GameManger.player.isLive = false;
+                    GameManger.currField.enemies.RemoveAll(x => x.Axis2D.x == this.Axis2D.x && x.Axis2D.y == this.Axis2D.y);
+                    return;
+                }
+                GameManger.currField.enemies.RemoveAll(x => x.Axis2D.x == this.Axis2D.x && x.Axis2D.y == this.Axis2D.y);
+            }
+        }
     }
 }
--- Project1st/Game/GameObject/Player.cs
+++ Project1st/Game/GameObject/Player.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Project1st.Game.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Project1st.Game.GameObject
 {
     public class Player : MoveObject
     {
+        public Timer attackTimer;
+
+        public int score;
+        public bool isCoolTime;
+        public int weapon;
+        public int viewCount;
+
+        public Player()
+        {
+            Init();
+        }
+        public Player(int x, int y)
+        {
+            Init();
+            Axis2D.x = x;
+            Axis2D.y = y;
+        }
+
+        public override void Init()
+        {
+            base.Init();
+            hitPoint = 100;
+            ID = 0;
+            isCoolTime = false;
+            score = 0;
+            weapon = 1;
+            viewCount = 3;
+        }
     }
 }
