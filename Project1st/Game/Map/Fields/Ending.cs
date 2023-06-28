d7fedaa6627c730c61b681849027bbabf4d3c587
DESKTOP-484M8JG\KGA <psc106@naver.com>
2023-06-28 13:41:26 +0900

    Init : 엔딩씬 클래스 생성
    승리/패배 출력용 클래스

---
+++ Project1st/Game/Map/Fields/Ending.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1st.Game.Map.Fields
+{
+    public class Ending : FieldBase
+    {
+        public Ending() { }
+        public Ending(FieldBase field) { }
+    }
+}
