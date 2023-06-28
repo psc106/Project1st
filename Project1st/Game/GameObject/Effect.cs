4b696f099a90e160ccdeb2c45f57900edde48bea
DESKTOP-484M8JG\KGA <psc106@naver.com>
2023-06-28 14:13:29 +0900

    Init : 이펙트 클래스 생성
    공격시 발생하는 이펙트를 보여주는 클래스

---
+++ Project1st/Game/GameObject/Effect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1st.Game.GameObject
+{
+    public class Effect : ObjectBase
+    {
+        public static string[] effectString = { "／", "€", "Δ", "⇔" };
+        public int type;
+
+        public Effect()
+        {
+            type = 0;
+        }
+
+        public Effect(int type)
+        {
+            this.type = type;
+        }
+
+        public Effect(int x, int y, int type)
+        {
+            Axis2D.x = x;
+            Axis2D.y = y;
+            this.type = type;
+        }
+    }
+}
