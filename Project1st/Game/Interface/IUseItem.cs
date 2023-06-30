b7531e0a512c666488441fde2083b6f41791cfed
DESKTOP-484M8JG\KGA <psc106@naver.com>
2023-06-30 21:28:24 +0900

    Init : 메뉴창 기능 사용용 인터페이스 생성
    장비, 아이템 사용 기능용 인터페이스 생성

---
+++ Project1st/Game/Interface/IUseItem.cs
@@ -0,0 +1,15 @@
+﻿using Project1st.Game.GameObject;
+using Project1st.Game.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1st.Game.Interface
+{
+    public interface IUseItem
+    {
+        bool UseItem(Items item, Wagon obj);
+    }
+}
